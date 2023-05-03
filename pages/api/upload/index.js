0699b7d900eb46d4624b6356b08f780ce677bcdd
Lucas Araujo <lucascdearaujo444@gmail.com>
2023-05-03 11:34:35 -0300

    implementação com serviço de nuvem AWS concluído, função de upload de imagens funcionando

--- pages/api/upload/index.js
+++ pages/api/upload/index.js
@@ -1,4 +1,9 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types";
 import multiparty from "multiparty";
+
+const bucketName = "lucasa0-nextjs-ecommerce";
 
 export default async function handle(req, res) {
 	const form = new multiparty.Form();
@@ -11,8 +16,32 @@ export default async function handle(req, res) {
 	});
 
 	console.log("length:", files.file.length);
-	console.log(fields);
-	return res.json("ok");
+	const client = new S3Client({
+		region: "sa-east-1",
+		credentials: {
+			accessKeyId: process.env.S3_ACCESS_KEY,
+			secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+		},
+	});
+
+	const links = [];
+	for (const file of files.file) {
+		const extension = file.originalFilename.split(".").pop();
+		const newFilename = `${Date.now()}.${extension}`;
+		await client.send(
+			new PutObjectCommand({
+				Bucket: bucketName,
+				Key: newFilename,
+				Body: fs.readFileSync(file.path),
+				ACL: "public-read",
+				ContentType: mime.lookup(file.path),
+			}),
+		);
+		const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+		links.push(link);
+	}
+
+	return res.json({ links });
 }
 
 export const config = {
